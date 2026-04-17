70cd1b1a747eecb5baa75d16f55f938f237b09a0
PS Prashant <psprashantanilgod@gmail.com>
2026-04-17 21:29:27 +0530

    push

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: [
-    'localhost',
-    '192.168.1.1',
-    // Add your local network IP here (example)
-    // '192.168.1.100',   // ← change to your computer's IP
-  ],
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
